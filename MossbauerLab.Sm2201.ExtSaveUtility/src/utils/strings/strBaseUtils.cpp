6e07364bfdf3ce4e0b4c87b25cc9e1e14bcd8245
Ushakov Michael <um.nix.user@gmail.com>
2020-01-07 13:39:35 +0300

    common string utils was renamed

---
+++ MossbauerLab.Sm2201.ExtSaveUtility/src/utils/strings/strBaseUtils.cpp
@@ -0,0 +1,1 @@
+#include "strBaseUtils.h"
---
+++ MossbauerLab.Sm2201.ExtSaveUtility/src/utils/strings/strBaseUtils.h
@@ -0,0 +1,24 @@
+#ifndef SM2201_SPECTRUM_SAVER_SRC_UTILS_STRING_BASE_UTILS_H
+#define SM2201_SPECTRUM_SAVER_SRC_UTILS_STRING_BASE_UTILS_H
+/* Utils for work with strings in application that are running on Windows 98
+ *
+ *
+ */
+#include <string>
+namespace MossbauerLab
+{
+	namespace Utils
+	{
+		namespace Strings
+		{
+			/*std::string ltrim(const char* str)
+			{
+				if (str == NULL)
+					return std::string();
+			}*/
+
+			//std::string rtrim(const char* str);
+		}
+	}
+}
+#endif
