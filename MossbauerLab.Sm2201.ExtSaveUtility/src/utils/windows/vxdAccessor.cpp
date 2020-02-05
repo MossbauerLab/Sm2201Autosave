f51f72ce6b4a01baccf3b88e6ba242df6f512bbe
Ushakov Michael <um.nix.user@gmail.com>
2020-02-05 15:53:15 +0500

    compile errors were fixed

--- MossbauerLab.Sm2201.ExtSaveUtility/src/utils/windows/vxdAccessor.cpp
+++ MossbauerLab.Sm2201.ExtSaveUtility/src/utils/windows/vxdAccessor.cpp
@@ -1,5 +1,6 @@
 #include "vxdAccessor.h"
 #include <winioctl.h>
+#include <tchar.h>
 
 MossbauerLab::Utils::Windows::VxDAccessor::VxDAccessor()
 {
@@ -14,12 +15,16 @@ MossbauerLab::Utils::Windows::VxDAccessor::~VxDAccessor()
 
 bool MossbauerLab::Utils::Windows::VxDAccessor::init()
 {
-    _hVxD = CreateFile("\\\\.\\io32port.vxd", 0, 0, NULL, 0, 
+    _hVxD = CreateFile(_T("\\\\.\\io32port.vxd"), 0, 0, NULL, 0, 
                        FILE_FLAG_DELETE_ON_CLOSE, NULL);
+
+    return _hVxD != INVALID_HANDLE_VALUE;
 }
 
 DWORD MossbauerLab::Utils::Windows::VxDAccessor::read(DWORD port, BYTE size)
 {
+    if( _hVxD == NULL)
+        return -1;
     return 0;
 }
 
